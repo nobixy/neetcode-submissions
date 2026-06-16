2c21a24f47f0cfdf0a3af4de56a88837cbb57ecd
Joseph Noblett <noblettjoe@gmail.com>
2026-06-16 13:09:44 -0400

    Add: python-multiple-parameters - submission-2

---
+++ Python For Beginners/python-multiple-parameters/submission-2.py
@@ -0,0 +1,15 @@
+def two_sum(var1, var2):
+    res = var1 + var2
+    print(res)
+
+
+def three_sum(var1, var2, var3):
+    res = var1 + var2 + var3
+    print(res)
+
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
