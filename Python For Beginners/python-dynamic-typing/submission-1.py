66051b89880e9e24a47fddd505e4cb6b645b2dfd
Joseph Noblett <noblettjoe@gmail.com>
2026-06-16 13:08:24 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 67
+print(type(variable))
+variable = 6.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello, world!"
+print(type(variable))
+variable = [6, 7]
+print(type(variable))
